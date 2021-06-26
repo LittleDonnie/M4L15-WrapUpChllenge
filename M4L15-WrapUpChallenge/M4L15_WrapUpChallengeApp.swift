14c5ed5ef255b3ce7ba316255da22723aa2ae0ec
Monty Montgomery <poweredbyplants@gmail.com>
2021-06-26 08:34:31 -0500

    Initial Commit

---
+++ M4L15-WrapUpChallenge/M4L15_WrapUpChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4L15_WrapUpChallengeApp.swift
+//  M4L15-WrapUpChallenge
+//
+//  Created by Monty Montgomery on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct M4L15_WrapUpChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
